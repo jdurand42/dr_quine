42d31f71be0f75ae2fde2ab287d53f2b0bfaa5a8
jukazekajou <jerome.d274@gmail.com>
2021-09-06 18:16:06 +0200

    Sully done, left to do: ASM

---
+++ C/Sully/Sully.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define S "#include <stdio.h>%1$c#include <stdlib.h>%1$c%1$c#define S %2$c%4$s%2$c%1$c%1$cint main()%1$c{%1$c	int d = %5$d;%1$c%1$c	char path[126];%1$c	char name[126];%1$c	char executable[126];%1$c	char compile[126];%1$c%1$c	sprintf(path, %2$cSully_%3$cd.c%2$c, d);%1$c	sprintf(name, %2$cSully_%3$cd%2$c, d);%1$c	sprintf(compile, %2$cgcc -Wall -Wextra -Werror -o %3$cs %3$cs%2$c, name, path);%1$c	sprintf(executable, %2$c./%3$cs%2$c, name);%1$c	FILE *file;%1$c	file = fopen(path, %2$cw%2$c);%1$c	if (!file)%1$c		return (1);%1$c	fprintf(file, S, 10, 34, 37, S, d - 1);%1$c	fclose(file);%1$c	system(compile);%1$c	if (d - 1)%1$c		system(executable);%1$c	return (0);%1$c}%1$c"
+
+int main()
+{
+	int d = 5;
+
+	char path[126];
+	char name[126];
+	char executable[126];
+	char compile[126];
+
+	sprintf(path, "Sully_%d.c", d);
+	sprintf(name, "Sully_%d", d);
+	sprintf(compile, "gcc -Wall -Wextra -Werror -o %s %s", name, path);
+	sprintf(executable, "./%s", name);
+	FILE *file;
+	file = fopen(path, "w");
+	if (!file)
+		return (1);
+	fprintf(file, S, 10, 34, 37, S, d - 1);
+	fclose(file);
+	system(compile);
+	if (d - 1)
+		system(executable);
+	return (0);
+}
