c39e4f90d94e1f6d30fdb4f1438e7c993375a249
jukazekajou <jerome.d274@gmail.com>
2021-09-24 14:42:26 +0200

    adding security in case of ./Sully_0

--- C/Colleen/Colleen.c
+++ C/Colleen/Colleen.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /*
-commentaire hors du main
+	commentaire hors du main
 */
 
 void f(char *s)
@@ -12,8 +12,8 @@ void f(char *s)
 int main()
 {
 	/*
-	commentaire dans le main
+		commentaire dans le main
 	*/
-	char *s = "#include <stdio.h>%1$c%1$c/*%1$ccommentaire hors du main%1$c*/%1$c%1$cvoid f(char *s)%1$c{%1$c	printf(s, 10, s, 34);%1$c}%1$c%1$cint main()%1$c{%1$c	/*%1$c	commentaire dans le main%1$c	*/%1$c	char *s = %3$c%2$s%3$c;%1$c	f(s);%1$c}%1$c";
+	char *s = "#include <stdio.h>%1$c%1$c/*%1$c	commentaire hors du main%1$c*/%1$c%1$cvoid f(char *s)%1$c{%1$c	printf(s, 10, s, 34);%1$c}%1$c%1$cint main()%1$c{%1$c	/*%1$c		commentaire dans le main%1$c	*/%1$c	char *s = %3$c%2$s%3$c;%1$c	f(s);%1$c}%1$c";
 	f(s);
 }
